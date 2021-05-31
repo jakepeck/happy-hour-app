98f74ef7424262795e0e1a81c6dede4ff1b32e52
Jake Peck <jakepeck44@gmail.com>
2021-05-31 11:29:50 -0400

    got basic get requests working

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.get('/', (req, res) => {
+  console.log(`Welcome to the home directory`)
+  res.send('this is the response in the / route')
+})
+
+app.get('/hello', (req, res) => {
+  console.log("You're in the /hello route handler!")
+  res.send('Howdy')
+})
+
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
 })
