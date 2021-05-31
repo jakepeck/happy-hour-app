1f09c0e640f454904ecceec9dc7f78a3f85dce3d
Jake Peck <jakepeck44@gmail.com>
2021-05-31 13:56:13 -0400

    got getAllHappyHours working but not sure how to get from database"

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
-const HappyHour = require('../models/happyhour')
-const HappyHourDeal = require('../models/happyhourdeal')
+// const HappyHour = require('../models/index')
+// const HappyHourDeal = require('../models/index')
+const { HappyHour, HappyHourDeal } = require('../models')
 
 const createHappyHour = async (req, res) => {
   try {
@@ -23,7 +24,7 @@ const createHappyHourDeal = async (req, res) => {
 
 const getAllHappyHours = async (req, res) => {
   try {
-    const happyHours = await HappyHour.find({})
+    const happyHours = await HappyHour.find()
     return res.status(200).json({ happyHours })
   } catch (error) {
     return res.status(500).send(error.message)
--- models/index.js
+++ models/index.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 const HappyHourSchema = require('./happyhour')
 const HappyHourDealSchema = require('./happyhourdeal')
 
-// const HappyHour = mongoose.model('happyhours', HappyHourSchema)
-// const HappyHourDeal = mongoose.model('happyhourdeals', HappyHourDealSchema)
+const HappyHour = mongoose.model('happyhours', HappyHourSchema)
+const HappyHourDeal = mongoose.model('happyhourdeals', HappyHourDealSchema)
 
-module.exports = {}
+module.exports = {
+  HappyHour,
+  HappyHourDeal
+}
