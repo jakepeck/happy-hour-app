28d5350e76af7cb9e8197e05d11b0e9a860bcc6a
Jake Peck <jakepeck44@gmail.com>
2021-06-01 02:42:20 -0500

    adding more controller functions

--- controllers/index.js
+++ controllers/index.js
@@ -31,8 +31,60 @@ const getAllHappyHours = async (req, res) => {
   }
 }
 
+const deleteHappyHour = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await HappyHour.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('Happy hour deleted')
+    }
+    throw new Error('Happy hour not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const updateHappyHour = async (req, res) => {
+  try {
+    const { id } = req.params
+    await HappyHour.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true },
+      (err, happyhour) => {
+        if (err) {
+          res.status(500).send(err)
+        }
+        if (!happyhour) {
+          res.status(500).send('Happy hour not found!')
+        }
+        return res.status(200).json(happyhour)
+      }
+    )
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getHappyHourById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const happyhour = await HappyHour.findById(id)
+    if (happyhour) {
+      return res.status(200).json({ happyhour })
+    }
+    return res
+      .status(404)
+      .send('Happy hour with the specified ID does not exists')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
   createHappyHour,
   createHappyHourDeal,
-  getAllHappyHours
+  getAllHappyHours,
+  deleteHappyHour,
+  updateHappyHour
 }
