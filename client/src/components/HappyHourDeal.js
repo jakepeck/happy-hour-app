bcf8079f8f9cf848b550045df141c4f2e27d0068
Jake Peck <jakepeck44@gmail.com>
2021-06-01 02:04:15 -0500

    created HappyHourCard and HappyHourDeal component

---
+++ client/src/components/HappyHourDeal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HappyHourDeal extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Happy Hour Deal</h1>
+      </div>
+    )
+  }
+}
