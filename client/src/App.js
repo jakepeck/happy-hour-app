7bab28998b8635bc77c230f2109b1155e82186dd
Jake Peck <jakepeck44@gmail.com>
2021-06-01 02:15:05 -0500

    working on gApp.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
 import './App.css'
 import { Route } from 'react-router-dom'
 import Home from './pages/Home'
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header>Testing Testing Testing</header>
-      <main>
-        <Route path="/" component={Home} />
-      </main>
-    </div>
-  )
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      happyhours: [],
+      happyhourdeals: [],
+      newhappyhour: '',
+      newhappyhourdeal: ''
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>Testing Testing Testing</header>
+        <main>
+          <Route path="/" component={Home} />
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App
