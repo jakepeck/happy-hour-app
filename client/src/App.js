3fb41194a5dfdc3d1362664f73c9429422e9caed
Jake Peck <jakepeck44@gmail.com>
2021-06-01 02:21:57 -0500

    adding in route switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import './App.css'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import React, {Component} from 'react'
+import Nav from './components/Nav'
+import AllHappyHours from './pages/AllHappyHours'
 
 export default class App extends Component {
   constructor(props) {
@@ -17,9 +19,14 @@ export default class App extends Component {
   render(){
     return (
       <div className="App">
-        <header>Testing Testing Testing</header>
+        <header>
+          <Nav />
+        </header>
         <main>
-          <Route path="/" component={Home} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/allhappyhours" component={AllHappyHours}/>
+          </Switch>
         </main>
       </div>
     )
--- client/src/pages/AllHappyHours.js
+++ client/src/pages/AllHappyHours.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class AllHappyHours extends Components {
+export default class AllHappyHours extends Component {
   render() {
     return (
       <div>
